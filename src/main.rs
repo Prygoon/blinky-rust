2ee42f0ecdd4748b923f1679b6c47d4241088797
Qwerty <i@dont.know>
2022-03-29 17:18:13 +0300

    Irq blinky

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,104 @@
-#![deny(unsafe_code)]
+// #![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-
+use core::borrow::BorrowMut;
 // Includes.
 use panic_halt as _;
 
-use nb::block;
+use stm32f1xx_hal as hal;
 
+use crate::hal::{
+    gpio::{gpiob, Output, PushPull},
+    pac::{interrupt, Interrupt, Peripherals, TIM2},
+    prelude::*,
+    timer::{CountDownTimer, Event},
+};
+
+use core::cell::RefCell;
+use cortex_m::{asm::wfi, interrupt::Mutex};
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+use embedded_hal::digital::v2::OutputPin;
+use stm32f1xx_hal::timer::Timer;
 
+// NOTE You can uncomment 'hprintln' here and in the code below for a bit more
+// verbosity at runtime, at the cost of throwing off the timing of the blink
+// (using 'semihosting' for printing debug info anywhere slows program
+// execution down)
+//use cortex_m_semihosting::hprintln;
 
+// A type definition for the GPIO pin to be used for our LED
+type LedPin = gpiob::PB1<Output<PushPull>>;
+
+// Make LED pin globally available
+static G_LED: Mutex<RefCell<Option<LedPin>>> = Mutex::new(RefCell::new(None));
+
+// Make timer interrupt registers globally available
+static G_TIM: Mutex<RefCell<Option<CountDownTimer<TIM2>>>> = Mutex::new(RefCell::new(None));
+
+// Define an interrupt handler, i.e. function to call when interrupt occurs.
+// This specific interrupt will "trip" when the timer tim2 times out
+#[interrupt]
+#[allow(non_snake_case)]
+unsafe fn TIM2() {
+    static mut LED: Option<LedPin> = None;
+    static mut TIM: Option<CountDownTimer<TIM2>> = None;
+
+    let led = LED.borrow_mut().get_or_insert_with(|| {
+        cortex_m::interrupt::free(|cs|
+            // Move LED pin here, leaving a None in its place
+            {
+                G_LED.borrow(cs).replace(None).unwrap()
+            })
+    });
+
+    let tim = TIM.borrow_mut().get_or_insert_with(|| {
+        cortex_m::interrupt::free(|cs| {
+            // Move LED pin here, leaving a None in its place
+            G_TIM.borrow(cs).replace(None).unwrap()
+        })
+    });
+
+    let _ = led.toggle();
+    let _ = tim.wait();
+}
+#[deny(non_snake_case)]
 #[entry]
 fn main() -> ! {
-    // Get access to the core peripherals from the cortex-m crate
-    let cortex_periph = cortex_m::Peripherals::take().unwrap();
-    // Get access to the device specific peripherals from the peripheral access crate
-    let device_periph = pac::Peripherals::take().unwrap();
+    let device_periph = Peripherals::take().unwrap();
 
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
-    let mut flash = device_periph.FLASH.constrain();
     let mut rcc = device_periph.RCC.constrain();
+    let mut flash = device_periph.FLASH.constrain();
+    let clocks = rcc
+        .cfgr
+        .sysclk(8.mhz())
+        .pclk1(8.mhz())
+        .freeze(&mut flash.acr);
 
-    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-    // `clocks`
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    // Acquire the GPIOB peripheral
+    // Configure PC13 pin to blink LED
     let mut gpiob = device_periph.GPIOB.split(&mut rcc.apb2);
-
-    // Configure gpio B pin 1 as a push-pull output. The `crl` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
     let mut led = gpiob.pb1.into_push_pull_output(&mut gpiob.crl);
-    // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cortex_periph.SYST, &clocks).start_count_down(1.hz());
+    let _ = led.set_high(); // Turn off
+
+    // Move the pin into our global storage
+    cortex_m::interrupt::free(|cs| *G_LED.borrow(cs).borrow_mut() = Some(led));
+
+    // Set up a timer expiring after 1s
+    let mut timer =
+        Timer::tim2(device_periph.TIM2, &clocks, &mut rcc.apb1).start_count_down(1.hz());
+    // timer.start(1.secs()).unwrap();
+
+    // Generate an interrupt when the timer expires
+    timer.listen(Event::Update);
+
+    // Move the timer into our global storage
+    cortex_m::interrupt::free(|cs| *G_TIM.borrow(cs).borrow_mut() = Some(timer));
+
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2);
+    }
 
     loop {
-        let _ = led.toggle();
-        block!(timer.wait()).unwrap();
+        wfi();
     }
 }
